f3d69091e0ad12a0db9729f38ca77594c280d045
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2025-02-28 16:44:25 -0700

    Adding config options to git-conflict plugin

--- lua/plugins/gitconflict.lua
+++ lua/plugins/gitconflict.lua
@@ -1,1 +1,12 @@
-return { "akinsho/git-conflict.nvim", version = "*", config = true }
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  lazy = false,
+  opts = {
+    default_mappings = true,
+    default_commands = true,
+  },
+  config = function(_, opts)
+    require("git-conflict").setup(opts)
+  end,
+}
